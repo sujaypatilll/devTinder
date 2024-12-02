03d33a03952cb6f44be08cfaa5534e489c8f0025
sujaypatilll <147255619+sujaypatilll@users.noreply.github.com>
2024-12-02 16:19:47 +0530

    exploring get post delete patch

--- src/app.js
+++ src/app.js
@@ -2,14 +2,34 @@ const express = require("express")
 
 const app = express()
 
+app.delete("/login",(req,res)=>{
+    res.send("deleted sucessfully")
+})
+
+app.get("/login",(req,res)=>{
+
+    const data = 
+    {
+        "firstname":"sujay",
+        "lastname":"patil",
+        "rollno":20,
+        "nesteddata":{
+            lastname:"patil",
+            status:"billinore",
+            income:"10cr",
+            degrees:["BE","Diploma","MTECH"]
+        }
+    }
+    res.send(data)
+})
+
+app.post("/login",(req,res)=>{
+    console.log("Data is saved succesfully twice")
+    res.send("Welcome to login page")
+})
+
+
 app.listen(7777,(req,res)=>{
     console.log("server listing successfully")
 })
 
-app.use("/",(req,res)=>{
-    res.send("hello devtinder")
-})
-
-app.get("/login",(req,res)=>{
-    res.send("hello login page sujay ")
-})
